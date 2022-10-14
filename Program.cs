59e7b439006857b668461f2173987b19bd64d646
NicolasMilanese <nicolasmilanese2004@gmail.com>
2022-10-14 16:40:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        string x;
+        int x1, acumulador;
+        acumulador=0;
+
+        Console.WriteLine("ingrese numero");
+        x=Console.ReadLine();
+        x1=Convert.ToInt32(x);
+
+        acumulador=x1;
+
+        for (int i = x1; i > 1; i--)
+        {
+            acumulador=acumulador*(i - 1);
+            
+            
+        }
+        Console.WriteLine("el factorial de " + x1  + " es " + acumulador);
+        }
+    }
+}
